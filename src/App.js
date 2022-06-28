73495d552d27c9b572d0b32b7e7285c9cac47249
jonsi-ranpariya <107756189+jonsi-ranpariya@users.noreply.github.com>
2022-06-28 18:56:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./components/Header";
+import Register from "./components/Register";
+import Feature from "./components/Features";
+import Integration from "./components/Integration";
+import Footer from "./components/Footer";
+
+function App()
+{
+    return(
+        <>
+            <Header />
+            <Register />
+            <Feature />
+            <Integration />
+            <Footer />
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/Integration.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+
+class Integration extends Component{
+
+    render()
+    {
+        return(
+            <>
+                <div className="about">
+                    <div className="container">
+
+
+                        <div className="row">
+                            <div className="col-6 p-25">
+                                <h1>Your channel program on steroids</h1>
+                                <p>
+                                    Tired of finding MSPs that can help you boost your channel sales? You're luck, because they're all here.
+                                </p>
+
+                            </div>
+                            <div className="col-6">
+                                <div className="about__img">
+                                    <img src="./img11.jpeg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                             <div className="col-6">
+                                <div className="about__img">
+                                    <img src="./img22.jpeg" alt="" />
+                                </div>
+
+                                
+                            </div>
+                            <div className="col-6 p-25">
+                                <h1>Become the vendor everyone wants to buy from</h1>
+                                <p>
+                                    Gain brand authority and visibility with the help of the largest IT service ecosystem. Find customers who are looking for your solution right now!
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-6 p-25">
+                                <h1>It's like having Your best Salesman on autopilot</h1>
+                                <p>
+                                    Share collaterals and documents that are automatically branded. LEverage a rich partner network that sells your solution exactly the way your best salesman would.
+                                </p>
+
+                            </div>
+                            <div className="col-6">
+                                <div className="about__img">
+                                    <img src="./img33.jpeg" alt="" />
+                                </div>
+
+                                
+                            </div>
+                        </div>
+
+                        <div className="row">
+                             <div className="col-6">
+                                <div className="about__img">
+                                    <img src="./img44.jpeg" alt="" />
+                                </div>
+
+                                
+                            </div>
+                            <div className="col-6 p-25">
+                                <h1>Track your channel performance</h1>
+                                <p>
+                                    Monitor the health and revenue of your cahnnel program an dstreamline your campaign to win more customers within Zomwntum.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-6 p-25">
+                                <h1>Get The best version of Your sales playbook</h1>
+                                <p>
+                                    Gather insights about top channel seller and find out what pitch and collaterals ensure maximum profitability. Win happy customer with an optimized sales playbook.
+                                </p>
+
+                            </div>
+                            <div className="col-6">
+                                <div className="about__img">
+                                    <img src="./img55.jpeg" alt="" />
+                                </div>
+
+                                
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Integration;
